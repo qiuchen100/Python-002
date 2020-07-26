aa8e1ce054cbdb850257d994e2d2ea81e484dcd9
qiuchen100 <qiuchen100@163.com>
2020-07-26 20:51:50 +0800

    add ch11

--- week01/crawler/crawler/spiders/movies.py
+++ week01/crawler/crawler/spiders/movies.py
@@ -1,6 +1,5 @@
 from urllib import parse
 import scrapy
-from bs4 import BeautifulSoup
 from scrapy import Selector
 from crawler.items import CrawlerItem
 
@@ -18,20 +17,18 @@ class MoviesSpider(scrapy.Spider):
             yield scrapy.Request(url, callback=self.parse)
 
     def parse(self, response):
-        soup = BeautifulSoup(response.text, 'html.parser')
-        movies = soup.find_all('div', attrs={'class': 'hd'})
+        movies = Selector(response).xpath('//div[@class="hd"]')
         for movie in movies:
             item = CrawlerItem()
-            movie_info = movie.find('a',)
-            title = movie_info.find('span', ).text
-            link = movie_info.get('href')
+            title = movie.xpath('./a/span/text()').extract_first().strip()
+            link = movie.xpath('./a/@href').extract_first().strip()
             item['title'] = title
             item['link'] = link
             yield scrapy.Request(link, meta={'item': item}, callback=self.parse2)
 
     def parse2(self, response):
         item = response.meta['item']
-        soup = BeautifulSoup(response.text, 'html.parser')
-        intro = soup.select('div.related-info')[0].get_text().strip()
+        intro = Selector(response).xpath(
+            '//div[@class="related-info"]').extract_first().strip()
         item['intro'] = intro
         yield item
